9cbce0a8b31a88d5088152e12e4d04b3ed82b4b9
Sidhant <sidhantpandit256@gmail.com>
2026-07-15 18:15:10 +0545

    [ADDED]:: added Cart [Models, Factories] file.

---
+++ database/factories/CartFactory.php
@@ -0,0 +1,17 @@
+<?php
+// database/factories/CartFactory.php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CartFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->customer(),
+        ];
+    }
+}

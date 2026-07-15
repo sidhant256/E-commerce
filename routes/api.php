f77da42147cb7d3857e3f34259ebbd6c70f0b44b
Sidhant <sidhantpandit256@gmail.com>
2026-07-15 13:24:32 +0545

    [UPDATED]:: updated API routes for AuthController files for customer, vendor, login and logout.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register/customer', [AuthController::class, 'registerCustomer']);
+Route::post('/register/vendor', [AuthController::class, 'registerVendor']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);

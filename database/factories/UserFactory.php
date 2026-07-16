c1e8cf9353381980d68969b7588ca9a0500d3104
Sidhant <sidhantpandit256@gmail.com>
2026-07-16 10:53:08 +0545

    [ADDED]::  added User Factory file.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,45 +1,45 @@
 <?php
+// database/factories/UserFactory.php
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
+            'role' => UserRole::Customer,
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function customer(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'role' => UserRole::Customer,
+        ]);
+    }
+
+    public function vendorUser(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::Vendor,
+        ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => UserRole::Admin,
         ]);
     }
 }
